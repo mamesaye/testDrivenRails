24cfba0f5872d4f8d5a9080360c9482dc8ae3281
Adama <mamesaye@hotmail.com>
2013-07-22 15:40:06 -0400

    refactor to not interact with session from controller and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #create a setter
+  def sign_in_as(email)
+     session[:current_email] = email
+  end
+  
+  def current_user
+     openStruct.new(email: session[:current_email])
+  end
+  
+  helper_method :current_user
 end
