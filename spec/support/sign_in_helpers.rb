99da86d7b6a904ad335785ee89a8f78f9b3a17a6
Adama <mamesaye@hotmail.com>
2013-07-26 16:57:39 -0400

    refactor sign in make it independant

---
+++ spec/support/sign_in_helpers.rb
@@ -0,0 +1,16 @@
+module SignInHelpers 
+  def sign_in
+     sign_in_as 'person@example.com'
+  end
+  
+  def sign_in_as(email)
+  visit root_path
+     fill_in 'Email address', with: email
+     click_button 'Sign In'
+  end
+  
+end
+
+Rspec.configure do |config| 
+  config.include SignInHelpers
+end
